3625aee3cc562cf505a1df70f54528e7681d72f6
Geronimo Perez <geronimo.perez4@gmail.com>
2023-08-07 02:19:16 -0400

    working on css

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,18 +4,28 @@ const handleClients = async () => {
     
     data = await ( await fetch('/api/data')).json();
 
+    console.log(data[0]);
+
     if(data) {
         main.innerHTML = '';
-        data.forEach(client => {
+        data.forEach(obj => {
+            let client = Object.keys(obj)[0];
+
+            console.log(client);
         main.innerHTML += `
         <div class="row">
             <h3>${client.toUpperCase()}</h3>
-            <button class="half">1/2 hour</button>
-            <button class="one">1 hour</button>
-            <button class="five">5 hours</button>
-            <button class="twenty">20 hours</button>
-            <button class="minusHalf">1/2 hour</button>
+            <div class="btnsDiv">
+                <button class="half">1/2 hour</button>
+                <button class="one">1 hour</button>
+                <button class="five">5 hours</button>
+                <button class="twenty">20 hours</button>
             </div>
+            <div class="remainDiv">
+                <button class="minusHalf">- 1/2 h</button>
+                <p class="remain">0</p>
+            </div>
+        </div>
             `;
         });
     };
@@ -24,11 +34,18 @@ const handleClients = async () => {
 handleClients();
 
 const addClient = () => {
+    
     if(newClient.value) {
-        data = [ newClient.value, ...data ];
-        writeFile('./assets/db/data.json', JSON.stringify(data), err => {
-            if(err) throw err;
-            handleClients();
-        });
+        data = [ {[newClient.value]:{hours:[0,0,0,0,0],dates:[],remain:0}}, ...data ];
+
+        console.log(JSON.stringify(data));
+
+        fetch('/api/data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(document.location.reload());
     };
 };
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const { log } = require('console');
 const express = require('express');
+const { writeFile } = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -6,7 +8,18 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get('/api/data', (req,res) => res.json(require('./db/data.json')));
 app.use(express.static('public'));
+
+app.get('/api/data', (req,res) => res.json(require('./db/data.json')));
+
+app.post('/api/data', ({body},res) => {
+
+    console.log('Data:',body);
+    
+    writeFile('./db/data.json', JSON.stringify(body), err => {
+        if(err) {console.log(err)};
+        res.status(200);
+    });
+});
 
 app.listen(PORT, () => console.log(`Now listeing on http://localhost:${PORT}`));
