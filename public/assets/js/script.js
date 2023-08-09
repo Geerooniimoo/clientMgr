fd9622191a9cf68a95fac84dc3486eb44a2047d9
Geronimo Perez <geronimo.perez4@gmail.com>
2023-08-09 14:57:53 -0400

    adding functionality to buttons

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,29 +4,29 @@ const handleClients = async () => {
     
     data = await ( await fetch('/api/data')).json();
 
-    console.log(data[0]);
-
     if(data) {
         main.innerHTML = '';
-        data.forEach(obj => {
-            let client = Object.keys(obj)[0];
-
-            console.log(client);
+        data.forEach(({id,name,hours,dates,remains}) => {
+        
+        let purchased = hours[0]*.5 + hours[1] + hours[2]*5 + hours[3]*10 + hours[4]*20;
+        let owed = purchased - dates.length*.5
+            
         main.innerHTML += `
         <div class="row">
             <div class="titleDiv">
-                <p class="title">${client.toUpperCase()}</p>
+                <p class="title">${name}</p>
                 <button class="filter">Filter</button>
             </div>
             <div class="btnsDiv">
-                <button class="half">1/2</button>
-                <button class="one">1</button>
-                <button class="five">5</button>
-                <button class="twenty">20</button>
+                <button onclick="addTime('${id}',0)" class="half">1/2</button>
+                <button onclick="addTime('${id}',1)" class="one">1</button>
+                <button onclick="addTime('${id}',2)" class="five">5</button>
+                <button onclick="addTime('${id}',3)" class="twenty">10</button>
+                <button onclick="addTime('${id}',4)" class="twenty">20</button>
             </div>
             <div class="remainDiv">
                 <button class="minusHalf">- 1/2 hour</button>
-                <p class="remain">0</p>
+                <p class="remain">${owed}</p>
             </div>
         </div>
             `;
@@ -36,19 +36,35 @@ const handleClients = async () => {
 
 handleClients();
 
-const addClient = () => {
+const addClient = async () => {
     
     if(newClient.value) {
-        data = [ {[newClient.value]:{hours:[0,0,0,0,0],dates:[],remain:0}}, ...data ];
+        data = [ 
+            {
+                remain: 0,
+                dates: [],
+                hours: [0,0,0,0,0],
+                name: newClient.value,
+                id: Math.floor(Math.random()*1000000).toString(16)
+            }, ...data 
+        ];
 
-        console.log(JSON.stringify(data));
-
-        fetch('/api/data', {
+        let newUser = await fetch('/api/data', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(document.location.reload());
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        });
+        
+        if(newUser.ok) document.location.reload();
     };
+};
+
+const addTime = async (id,i) => {
+    let timeAdded = await fetch(`/api/data/${i}`, {
+        method: 'PUT',
+        body: JSON.stringify({"id":id}),
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    if(timeAdded.ok) document.location.reload();
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const { log } = require('console');
 const express = require('express');
 const { writeFile } = require('fs');
+let store = require('./db/data.json');
 
 const app = express();
 const PORT = 3000;
@@ -10,16 +10,27 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.static('public'));
 
-app.get('/api/data', (req,res) => res.json(require('./db/data.json')));
+app.get('/api/data', (req,res) => {res.json(store)});
 
 app.post('/api/data', ({body},res) => {
-
-    console.log('Data:',body);
     
     writeFile('./db/data.json', JSON.stringify(body), err => {
         if(err) {console.log(err)};
         res.status(200);
     });
+});
+
+app.put('/api/data/:index', ({body,params},res) => {
+
+    console.log(body,params.index);
+
+    res.json('');
+
+    
+    // writeFile('./db/data.json', JSON.stringify(body), err => {
+    //     if(err) {console.log(err)};
+    //     res.status(200);
+    // });
 });
 
 app.listen(PORT, () => console.log(`Now listeing on http://localhost:${PORT}`));
